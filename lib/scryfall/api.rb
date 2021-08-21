33d0ac7c9552ad1a293ff391d55c77e01dc20092
Tony Noecker <tony@noecker.me>
2021-08-21 18:21:40 -0500

    add post

--- lib/scryfall/api.rb
+++ lib/scryfall/api.rb
@@ -19,6 +19,16 @@ module Scryfall
         JSON.parse res
       end
     end
+    
+    def post(path = "", body)
+      res = HTTP.post(mount_uri(path), body: body.to_json)
+
+      if args.has_key?(:to_struct) && args[:to_struct] == true
+        JSON.parse res, object_class: OpenStruct
+      else
+        JSON.parse res
+      end
+    end
 
     private
 
